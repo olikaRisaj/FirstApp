411133041c61409c131d522823c429f703299496
khorbach <kostiantyn.horbach@nordstrom.com>
2021-11-06 23:00:56 +0200

    init commit

---
+++ app/src/main/java/com/example/applicationrisaj/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.applicationrisaj
+
+object Constants {
+    const val appPreferences = "mySettings"
+    const val appPreferencesName = "username"
+    const val appPreferencesPassword = "userPassword"
+}
